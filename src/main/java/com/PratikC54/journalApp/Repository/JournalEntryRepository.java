2d044bd4a1b915b4d29f6b3a917f3de81ed8421d
PratikC54 <pratiksenapati54@gmail.com>
2025-10-26 23:23:38 +0530

    .

--- src/main/java/com/PratikC54/journalApp/Repository/JournalEntryRepository.java
+++ src/main/java/com/PratikC54/journalApp/Repository/JournalEntryRepository.java
@@ -4,7 +4,6 @@ import com.PratikC54.journalApp.Entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
 
 public interface JournalEntryRepository extends MongoRepository<JournalEntry,ObjectId> {
 
